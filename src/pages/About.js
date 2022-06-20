ed4eaee34dea8f84b5324f47f95e884d305dd615
Jafet160 <jafet.hernandez1@upr.edu>
2022-06-20 19:08:55 -0400

    week 2 changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile_pic from "../assets/profile_pic.jpeg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
             <img
               className="profile_image"
               src={profile_pic}
               alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              ></img>
             </div>
           </div>
-        </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Jafet Hernandez</div>
+              <div className="brief_description">
+                Hello! I am a sophomore student at the University of Puerto Rico, Mayagüez.
+                I am part of Puerto Rico's Water Polo National Team and I love to eat
+                Mexican food. I have many musical interests but I especially like dancing to "bachata". 
+                Have a wonderful day!
+              </div>
+            </div>
+          </div>
       </div>
-    );
+    )
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,14 @@ class Home extends Component {
       todos: [],
     };
   }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+      });
+    this.setState({
+      todos: todos,
+      });
+  };
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -20,12 +28,16 @@ class Home extends Component {
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
-    this.setState({
-      todos: new_list,
-    });
+    if (this.state.todos.find(duplicate => duplicate.content.trim() === todo.content.trim())) {
+      return;
+    } else {
+      // Create a array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+      // Update the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+    } 
   };
   render() {
     return (
@@ -36,7 +48,7 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
